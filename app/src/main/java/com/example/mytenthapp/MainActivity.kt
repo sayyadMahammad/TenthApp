9df0b8e9a2ce21a4b676b7a293484ee1de64fc85
Sayyad Mahammad <sayyadmahammad.byjus@aesl.in>
2022-12-12 11:55:29 +0530

    added app centre dependecies

---
+++ app/src/main/java/com/example/mytenthapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mytenthapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "c2a28a31-20e7-499e-a733-00ffad017161",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
